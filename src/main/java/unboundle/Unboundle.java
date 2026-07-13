92be5eea6c9568eae23169585f6563f19d19b1b0
Skyros4 <skyros.four@gmail.com>
2026-07-13 14:39:49 +0200

    Dispenser behaviour now also applies to colored bundles.

--- src/main/java/unboundle/Unboundle.java
+++ src/main/java/unboundle/Unboundle.java
@@ -1,6 +1,7 @@
 package unboundle;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.DispenserBlock;
 
@@ -10,6 +11,30 @@ public class Unboundle implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		UnboundleConfig.load();
+		//? if >= 26.2 {
+		/*DispenserBlock.registerBehavior(Items.BUNDLE, new BundleDispenseItemBehavior());
+		for (DyeColor color : DyeColor.values()) {
+			DispenserBlock.registerBehavior(Items.DYED_BUNDLE.pick(color), new BundleDispenseItemBehavior());
+		}
+		*///?} else {
 		DispenserBlock.registerBehavior(Items.BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.WHITE_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.ORANGE_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.MAGENTA_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.LIGHT_BLUE_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.YELLOW_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.LIME_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.PINK_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.GRAY_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.LIGHT_GRAY_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.CYAN_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.PURPLE_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.BLUE_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.BROWN_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.GREEN_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.RED_BUNDLE, new BundleDispenseItemBehavior());
+		DispenserBlock.registerBehavior(Items.BLACK_BUNDLE, new BundleDispenseItemBehavior());
+		 //?}
+
 	}
 }
